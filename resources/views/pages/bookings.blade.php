cf6bb0c3a6d8e0956e2f190b7fbfb84f7de04950
thetti27 <liveup2it>
2022-05-02 22:37:56 +0530

    Change s to booking page

--- resources/views/pages/bookings.blade.php
+++ resources/views/pages/bookings.blade.php
@@ -5,6 +5,21 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Bookings') }}
         </h2>
+
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('I am going to write some code here. ') }}
+        </h2>
+
+
+        <form action="/action_page.php">
+            <label for="fname">First name:</label><br>
+            <input type="text" id="fname" name="fname" value="John"><br>
+            <label for="lname">Last name:</label><br>
+            <input type="text" id="lname" name="lname" value="Doe"><br><br>
+            <input type="submit" value="Submit">
+          </form>
+
+
     </x-slot>
 
 
